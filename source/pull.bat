6fc6bd3efcf1b06a1851d452d7caa2f37e949620
Chase <36320938+llxiaoyuan@users.noreply.github.com>
2023-09-23 18:39:11 +0800

    update

---
+++ source/pull.bat
@@ -0,0 +1,2 @@
+curl --remote-name "https://raw.githubusercontent.com/llxiaoyuan/my_sprint/main/my_sprint.h"
+curl --remote-name "https://raw.githubusercontent.com/llxiaoyuan/my_sprint/main/my_sprint.c"
